48a94ab8dd81780c00cd961f63253d3c94609288
shoichi.osaki <shoichi.osaki@mail.rakuten.com>
2013-06-27 18:34:46 +0900

    PORT変更

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
   res.end("Hello worldn");
 });
-server.listen(process.env.PORT || 8001);
+server.listen(process.env.PORT || 80);
